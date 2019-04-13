4ea116f44b7141c5d6207907f9c4e93d56155f72
ravila4 <ravila@protonmail.com>
2019-04-12 22:38:44 -0600

    Rivem path is relative to install folder.

--- RIVEM/__init__.py
+++ RIVEM/__init__.py
@@ -4,7 +4,7 @@ Last edited: 2019-03-31
 
 import subprocess
 import tempfile
-import os
+from os import path
 
 
 RIVEM_version = '4.5'
@@ -15,13 +15,16 @@ class rivem():
     def __init__(self):
         # Output file is generated in a temp dir
         self.temp_dir = tempfile.mkdtemp()
-        self.out = os.path.join(self.temp_dir, "map.ps")
+        self.out = path.join(self.temp_dir, "map.ps")
         self.PDB = None
         self.matrix = None
+        # Path to rivem executable
+        self.rivem_path = path.join(path.dirname(__file__), "rivem")
+
 
     def generate_cmd(self):
         """Generate the argument list from the class attributes."""
-        cmd = ["rivem"]
+        cmd = [self.rivem_path]
         if self.PDB is not None:
             cmd += ["-p", self.PDB]
         if self.matrix is not None:
