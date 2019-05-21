28bf7e9b3b6d64a42c3b4e00e12425171f194856
ravila4 <ravila@protonmail.com>
2019-05-20 21:31:28 -0600

    Add variables for residue labels. Add variables for plot region. Convert command to string list.

--- RIVEM/__init__.py
+++ RIVEM/__init__.py
@@ -15,6 +15,21 @@ class rivem():
         self.out = None
         self.PDB = None
         self.matrix = None
+        self.label = None
+        self.label_size = None
+        self.label_color = None
+        self.polar = 1
+        self.begPhi = 0.0
+        self.endPhi = 180.0
+        self.begTheta = 0.0
+        self.endTheta = 180.0
+        self.deltaAngle = 1.0
+        self.begX = -3.402823e+38
+        self.endX = 3.402823e+38
+        self.begY = -3.402823e+38
+        self.endY = 3.402823e+38
+        self.begZ = -3.402823e+38
+        self.endZ = 3.402823e+38
         self.color_method = None
         self.dmin = None
         self.dmax = None
@@ -30,6 +45,34 @@ class rivem():
             cmd += ["-p", self.PDB]
         if self.matrix is not None:
             cmd += ["-m", self.matrix]
+        if self.label is not None:
+            cmd += ["-l"]
+            if self.label_size is not None:
+                cmd += [self.label_size]
+            if self.label_color is not None:
+                cmd += [self.label_color]
+        if self.begPhi != 0.0:
+            cmd += ["-Pbeg", self.begPhi]
+        if self.endPhi != 180.0:
+            cmd += ["-Pend", self.endPhi]
+        if self.begTheta != 0.0:
+            cmd += ["-Tbeg", self.begTheta]
+        if self.endTheta != 180.0:
+            cmd += ["-Tend", self.endTheta]
+        if self.deltaAngle != 1.0:
+            cmd += ["-D", self.deltaAngle]
+        if self.begX != -3.402823e+38:
+            cmd += ["-Xbeg", self.begX]
+        if self.endX != 3.402823e+38:
+            cmd += ["-Xend", self.endX]
+        if self.begY != -3.402823e+38:
+            cmd += ["-Ybeg", self.begY]
+        if self.endY != 3.402823e+38:
+            cmd += ["-Yend", self.endY]
+        if self.begZ != -3.402823e+38:
+            cmd += ["-Zbeg", self.begZ]
+        if self.endZ != 3.402823e+38:
+            cmd += ["-Zend", self.endZ]
         if self.color_method is not None:
             cmd += ["-c", self.color_method]
             if self.dmin is not None:
@@ -42,7 +85,7 @@ class rivem():
                 cmd += ["-g", self.color_min]
         # Add output file
         cmd += ["-O", self.out]
-        return cmd
+        return [str(k) for k in cmd]
 
     def run(self):
         """Run the rivem executable."""
